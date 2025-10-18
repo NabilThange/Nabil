5516baeed5772e1e796be0b30c640d2e267336b6
NabilThange <thangenabil@gmail.com>
2025-10-18 17:53:40 +0530

    gsc 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,6 +52,8 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         
+        <meta name="google-site-verification" content="ECVhL-uOKKAR4INSHOcwPxBFg9lULvqSJD2qwx4viTQ" />
+        
         {/* Viewport optimization for mobile-first indexing */}
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5" />
         
